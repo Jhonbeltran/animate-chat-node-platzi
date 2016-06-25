191ed7052b483067de66cabb483afa10ca2c0c53
Jhonbeltran <jbeltranleon@gmail.com>
2016-06-25 14:33:27 -0500

    Refactor: Utilizo streams en fs En este commit hacemos un refactor para utilizar Streams, uno de los conceptos mas poderosos de io.js / Node.
    En el anterior paso el archivo de carga todo en el buffer y cuando este
    termina de cargarse lo pasa al response cuando esta listo. Con streams a
    medida que se va leyendo el archivo este lo va pasando al responde y el
    performance es mucho mejor (Claro esta, que es visto cuando se trabaja
    con archivos mas grandes)
    
    A medida de este curso vamos a utilizar los 3 conceptos: callbacks,
    event emitter y streams para potenciar nuestra programación asíncrona
    con io.js

--- server.js
+++ server.js
@@ -15,28 +15,33 @@ const path = require('path')
 
 
 function onRequest(req, res) {
-	//Ahora lo correcto es trabajar con funcionalidades asincronas :)
-	//El ultimo elemento de una funcion asincrona es un callback
-	//En este caso la funcion dentro del readFile va a recibir el archivo despues de que lo cargue
-
-	//Vamos a usar un callback muy comun en node "El error como primer argumento"
-
-	//Recordemos que el let es usado en vez de var para definir variables con un scope seguro
-	//Para que la variable solo exista dentro de su hambito y no pueda ser usada fuera de el.
-	//let para variables, const para constantes
-
 	let fileName = path.join(__dirname, 'public', 'index.html')
 
-	fs.readFile(fileName, function(err, file){
-		if (err){
-			//Asi manejamos el error 
-			return res.end(err.message)
-		}
-		//Le indico al navegador que tipo de archivo es, aunque el normalmente ya entiende
-		res.setHeader('Content-Type', 'text/html')
-		res.end(file)
+	/*Streams: Mecanismos para poder leer archivos o fuentes de informacion binaria,
+	de forma inteligente, es decir una lectura mucho mas rapida, vamos a usar un stream de lectura
+	y enviarlo a un stream de escritura*/
+
+	//Creamos un rs = readstream a partir de fileSystem fs
+	let rs = fs.createReadStream(fileName)
+
+	//Seteamos el header
+	res.setHeader('Content-Type', 'text/html')
+
+	//req y res tambien son streams .___. 
+	//De esta manera (la liena de abajo) ya estoy leyendo el archivo y lo estoy enviando
+	//.pipe es una tuberia ._.
+	rs.pipe(res)
+	
+	//Si hay errores los podemos controlar de la siguente forma
+
+	rs.on('error', function (err) {
+		//No necesitamos el return ._.
+		res.end(err.message)
 	})
+	/* Los streams nos permiten manipular canales de informacion haciendo buffering inteligente
+	sin necesidad de tener que cargar todo el bloque en memoria*/
 }
+
 function onListening(){
 	console.log('☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢ ☢')
 	console.log('El servidor ha iniciado y está escuchando en el puerto: ' + port)
