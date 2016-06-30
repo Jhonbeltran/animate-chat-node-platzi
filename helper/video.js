8702bf1b8ce1ce13701f43e96f89583649da82c1
Jhonbeltran <jbeltranleon@gmail.com>
2016-06-29 22:49:20 -0500

    Decodifico imagenes y las guardo en el disco El primer paso para convertir las imagenes a video es realizar la decodificacion y guardar la imagen en el disco duro, para eso utilizo el modulo 'data-uri-to-buffer', este buffer lo almacenamos en nuestro sistema de archivos utilizando streams del modulo 'fs'. el directorio donde lo guardo es el temporal del sistema y para evitar conflictos con otros usuarios convirtiendo imagenes utilizaremos el modulo 'uuid' para generar un nombre unico. Ya que las imagenes son un arreglo vamos a utilizar la funcion 'async.eachSeries' para aplicar la decodificacion a cada una de las imagenes en orden.

--- helper/video.js
+++ helper/video.js
@@ -9,10 +9,28 @@ const async = require('neo-async')
 //Requerimos data-uri-to-buffer para hacer la conversion de imagenes
 const dataURIBuffer= require('data-uri-to-buffer')
 
+//Requerimos uuid para que nos ayude con la generacion de nombres de imagenes unicos
+const uuid = require('uuid')
+
+//Requerimos un modulo del core de node para acceder a algunas funcionalidades deo os
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
+
+
 //Vamos a exportar una funcion
 module.exports= function(images) {
 	//Generamos un gestor de eventos
 	let events = new EventEmitter()
+
+	//Contador de las imagenes que vamos a estar trasnformando
+	let count = 0
+
+	//El nombre que vemos a generar con el uuid
+	let baseName = uuid.v4()
+
+	//Directorio temporal de imagenes
+	let tmpDir = os.tmpDir()
 
 	//Vamos a definir el workflow inicial de lo que vamos a trabajar
 	async.series([
@@ -29,12 +47,30 @@ module.exports= function(images) {
 		//Usamos .eachSeries para asegurar el orden de las imagenes
 		//El primer argumento que recibe es un arreglo, despues un callback
 		async.eachSeries(images, decodeImage, done)
-		done()
+		
 	}
 
 	function decodeImage(image, done) {
 		// Vamos a convertir la imagen a buffer 
 		//y la vamos a guardar en el sistema de archivos con un nombre (unico)
+		//para fileName usamos template strings (será un nombre dinamico)
+		let fileName = `${baseName}-${count++}.jpg`
+
+		let buffer = dataURIBuffer(image)
+		//Usaremos un stream de escritura
+		let ws = fs.createWriteStream(path.join(tmpDir, fileName))
+
+		/*Como el eventemitter pasa como primer argumento el error,
+		 el callback va a ver como primer argumento un error,
+		 asi que va a romper la cadena de ejecucion, 
+		 de esta manera con streams vamos a poder parar la cadena de llamado asincrono*/
+		ws.on('error', done)
+		/*Terminamos el stream pasandole el buffer y cuando termine de pasar el buffer,
+		vamos a ejecutar el metodo done*/
+		ws.end(buffer, done)
+
+		events.emit('log', `Converting ${fileName}`)
+
 
 	}
 	
