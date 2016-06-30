46efd469342f059f431460231977a7f7d4570d8f
Jhonbeltran <jbeltranleon@gmail.com>
2016-06-29 20:44:19 -0500

    Utilizo neo-async en las funciones base que he creado en el helper En este commit creamos el workflow básico para realizar la conversion de video, para este caso utilizaremos el módulo  que nos facilitará el trabajo con las funciones asíncronas para procesar las imagenes y crear el video.
    Inicialmente usaremos  que ejecuta un arreglo de funciones
    en serie, esto es, la siguiente función se ejecuta solo cuando la
    primera termina su ejecución. También usaremos funciones con nombre para
    evitar code smells y tener un código mas mantenible. (la funcion done() es la que nos ayuda a pasar de funcion en funcion dentro de .series

--- helper/video.js
+++ helper/video.js
@@ -12,29 +12,48 @@ module.exports= function(images) {
 	let events = new EventEmitter()
 
 	//Vamos a definir el workflow inicial de lo que vamos a trabajar
+	/*Con series definimos cuales son las funciones que se van a ejecutar(de manera secuencial),
+	la ultima funcion convertFinished la usamos a modo de callback*/
+	async.series([
+		decodeImages,
+		createVideo,
+		encodeVideo,
+		cleanup
+	], convertFinished)
 
 	//Debo decodificar las imagenes de base64(Data uri) a buffer(formato binario)
 	//Buffer me permite pasar facilmente las imagenes al sistema de archivos y almacenarlas
 	//como jpeg
-
-	function decodeImages() {}
+	function decodeImages(done) {
+		//Done es un callback el cual continua la ejecucion a la siguiente funcion
+		done()
+	}
 	
 	//Creamos el video con las imagenes
-	function createVideo() {}
+	function createVideo(done) {
+		done()
+	}
 	
 	//Codifico el video al formato de la web
-	function encodeVideo() {}
+	function encodeVideo(done) {
+		done()
+	}
 	
 	//Limpieza de archivos temporales
-	function cleanup() {}
+	function cleanup(done) {
+		done()
+	}
 
-	function convertFinished(){}
-
-
-	setTimeout(function(){
+	//La ultima, como es el callback final, solamente va a recibir el error.
+	function convertFinished(err){
+		setTimeout(function(){
 		//De esta forma emitimos el evento
 		events.emit('video', 'this will be the encoded video')
-	}, 5000)
+		}, 5000)
+	}
+
+
+	
 
 	return events
 }
